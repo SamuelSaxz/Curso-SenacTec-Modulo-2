07b1cbde4562c2a93711f7c641c88805672b28b3
Samuel de Almeida Aguilar <samueldealmeida.aguilar12@gmail.com>
2024-07-12 21:01:44 -0300

    Criando Include Header

---
+++ include/header.php
@@ -0,0 +1,85 @@
+<header>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid d-flex gap-4">
+      <a class="navbar-brand" href="#">EADS Ltda</a>
+      <a href="/samuelAguilar/Login.php" class="nav-link">Login</a>
+      <a href="/samuelAguilar/Cadastro.php" class="nav-link">Cadastrar</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Funções
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="/samuelAguilar/index.php?pagina=segundoGrau">Função do 2° Grau</a>
+              </li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <li><a class="dropdown-item" href="/samuelAguilar/index.php?pagina=Imc">Função do IMC</a></li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+          </li>
+          <li><a class="dropdown-item" href="/samuelAguilar/index.php?pagina=Forca">Função para Força</a></li>
+          <li>
+            <hr class="dropdown-divider">
+          </li>
+          <li><a class="dropdown-item" href="/samuelAguilar/index.php?pagina=Fatorial">Função para Fatorial</a></li>
+          <li>
+            <hr class="dropdown-divider">
+          </li>
+          <li><a class="dropdown-item" href="/samuelAguilar/index.php?pagina=Bissexto">Função para Ano Bissexto</a>
+          </li>
+          <li>
+            <hr class="dropdown-divider">
+          </li>
+          <li><a class="dropdown-item" href="/samuelAguilar/index.php?pagina=JurosSimples">Função para Juro
+              Simples</a></li>
+          <li>
+            <hr class="dropdown-divider">
+          </li>
+          <li><a class="dropdown-item" href="/samuelAguilar/index.php?pagina=Km">Função para Km</a></li>
+          <li>
+            <hr class="dropdown-divider">
+          </li>
+          <li><a class="dropdown-item" href="/samuelAguilar/index.php?pagina=Multiplicar">Função para Multiplicar</a>
+          </li>
+          <li>
+            <hr class="dropdown-divider">
+          </li>
+          <li><a class="dropdown-item" href="/samuelAguilar/index.php?pagina=Login">Função para Login</a></li>
+          <li>
+            <hr class="dropdown-divider">
+          </li>
+          <li><a class="dropdown-item" href="/samuelAguilar/index.php?pagina=Register">Função para Cadastro</a></li>
+          <li>
+            <hr class="dropdown-divider">
+          </li>
+          <li><a class="dropdown-item" href="/samuelAguilar/index.php?pagina=Banco">Função para Banco</a></li>
+          <li>
+            <hr class="dropdown-divider">
+          </li>
+          <li><a class="dropdown-item" href="/samuelAguilar/index.php?pagina=PrimeNumber">Função para Número Primo</a>
+          </li>
+          <li>
+            <hr class="dropdown-divider">
+          </li>
+          <li><a class="dropdown-item" href="/samuelAguilar/index.php?pagina=OutrosMetodos&pasta=/POLIMORFISMO">Outros
+              Metodos</a></li>
+          </li>
+        </ul>
+        </li>
+        </ul>
+        <form class=" col-6 d-flex justify-content-center align-items-center" role="search"></form>
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+</header>
