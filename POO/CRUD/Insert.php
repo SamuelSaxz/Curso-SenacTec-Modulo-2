74868fee1b1b9f9909b6239102202e68487d06d2
Samuel de Almeida Aguilar <samueldealmeida.aguilar12@gmail.com>
2024-07-12 21:03:29 -0300

    Update Insert.php

--- POO/CRUD/Insert.php
+++ POO/CRUD/Insert.php
@@ -1,12 +1,15 @@
 <?php
 include_once 'Database.php';
-class Insert extends Database {
-  
-  public function __construct(string $table, array $column, array $value, int $columnDuplicate = null) {
-    parent::__construct($table, $column, $value, $columnDuplicate);
+class Insert extends Database
+{
+
+  public function __construct(string $table, array $column, array $value, int $columnDuplicate = null, array $where = [])
+  {
+    parent::__construct($table, $column, $value, $columnDuplicate, $where);
   }
 
-  private function Insert() {
+  private function Insert()
+  {
     $columns = implode(", ", $this->getColumn());
     $values = implode("', '", $this->getValue());
     $query = "INSERT INTO $this->table ($columns) VALUES ('$values')";
@@ -14,32 +17,24 @@ class Insert extends Database {
     echo "Usuário cadastrado com sucesso!";
   }
 
-  private function verifyDuplicate() {
+  private function verifyDuplicate()
+  {
     $sql = $this->selectWhere($this->getTable(), $this->getColumn()[$this->getColumnDuplicate()], $this->getValue()[$this->getColumnDuplicate()], true);
     if ($sql > 0) {
       echo $this->getColumn()[$this->getColumnDuplicate()] . " já cadastrado!";
-    }
-    else {
+    } else {
       return $this->Insert();
     }
   }
 
-  public function execInsert() {
+  public function execInsert()
+  {
     echo "Cadastrando usuário... <br>";
     if ($this->getColumnDuplicate()) {
       echo "Verificando se o e-mail já existe... <br>";
       return $this->verifyDuplicate();
-    }
-    else {
+    } else {
       return $this->Insert();
     }
   }
 }
-
-$insert = new Insert(
-  'users', 
-  ['name', 'email', 'password'], 
-  ['Samuel de Almeida Aguilar', 'teste3@gmail.com', 'teste1234'], 
-  1
-);
-$insert->execInsert();
